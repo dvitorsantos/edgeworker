2de1562eb39d92f52431412a57fe2ad909ef715d
Vitor Santos <vitoralannl@gmail.com>
2023-03-11 08:09:16 -0300

    Initial commit

---
+++ src/main/java/lsdi/edgeworker/EdgeworkerApplication.java
@@ -0,0 +1,13 @@
+package lsdi.edgeworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsdi/edgeworker/EdgeworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package lsdi.edgeworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
